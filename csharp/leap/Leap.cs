40107762f9fdd0bf51a6ce23d50644ab1c873a24
Lucas Lira <lucasgta95@gmail.com>
2020-02-15 18:00:43 -0300

    added more

---
+++ csharp/leap/Leap.cs
@@ -0,0 +1,26 @@
+using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+
+        if (year % 4 != 0)
+        {
+            return false;
+        }
+
+        else if (year % 100 == 0)
+        {
+            return false;
+        }
+
+        else if (year % 100 == 0 && year % 400 == 0)
+        {
+            return true;
+        }
+
+        else return true;
+		
+    }
+}
