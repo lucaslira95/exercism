410c36b2990ba762233956ab0580514f0e30f425
Lucas Lira <lucasgta95@gmail.com>
2020-05-18 19:21:36 -0300

    refactor: Changes based on code conventions

--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -1,26 +1,30 @@
+#ifndef _SPACE_AGE_H
+#define _SPACE_AGE_H
+
 namespace space_age {
     class space_age{
         private:
-            const long Seconds;
-            const double Earth = 31557600.0;
-            const double Mercury = Earth * 0.2408467;
-            const double Venus = Earth * 0.61519726;
-            const double Mars = Earth * 1.8808158;
-            const double Jupiter = Earth * 11.862615;
-            const double Saturn = Earth * 29.447498;
-            const double Uranus = Earth * 84.016846;
-            const double Neptune = Earth * 164.79132;
+            const long long int Seconds;
+            static constexpr double EarthYearInSeconds = 31557600.0;
+            static constexpr double MercuryYearInSeconds = EarthYearInSeconds * 0.2408467;
+            static constexpr double VenusYearInSeconds = EarthYearInSeconds * 0.61519726;
+            static constexpr double MarsYearInSeconds = EarthYearInSeconds * 1.8808158;
+            static constexpr double JupiterYearInSeconds = EarthYearInSeconds * 11.862615;
+            static constexpr double SaturnYearInSeconds = EarthYearInSeconds * 29.447498;
+            static constexpr double UranusYearInSeconds = EarthYearInSeconds * 84.016846;
+            static constexpr double NeptuneYearInSeconds = EarthYearInSeconds * 164.79132;
 
         public:
-            space_age(long num) : Seconds(num){}
+            explicit space_age(long long int age_in_seconds) : Seconds(age_in_seconds){}
             long seconds() const { return Seconds; }
-            double on_earth() const {return Seconds/Earth;}
-            double on_mercury() const {return Seconds/Mercury;}
-            double on_venus() const {return Seconds/Venus;}
-            double on_mars() const {return Seconds/Mars;}
-            double on_jupiter() const {return Seconds/Jupiter;}
-            double on_saturn() const {return Seconds/Saturn;}
-            double on_uranus() const {return Seconds/Uranus;}
-            double on_neptune() const {return Seconds/Neptune;}
+            double on_earth() const {return Seconds/EarthYearInSeconds;}
+            double on_mercury() const {return Seconds/MercuryYearInSeconds;}
+            double on_venus() const {return Seconds/VenusYearInSeconds;}
+            double on_mars() const {return Seconds/MarsYearInSeconds;}
+            double on_jupiter() const {return Seconds/JupiterYearInSeconds;}
+            double on_saturn() const {return Seconds/SaturnYearInSeconds;}
+            double on_uranus() const {return Seconds/UranusYearInSeconds;}
+            double on_neptune() const {return Seconds/NeptuneYearInSeconds;}
     };
 }
+#endif
